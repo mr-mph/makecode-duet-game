a41ac13f6111574d87c9241a138709d8531ae387
Seth Williams <88854453+mr-mph@users.noreply.github.com>
2024-06-23 16:05:14 -0700

    add dynamic background, increase rot speed

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 namespace SpriteKind {
     export const Background = SpriteKind.create()
+    export const Static = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-    sprite.startEffect(effects.disintegrate, 100)
+    game_over = true
+    sprite.startEffect(effects.disintegrate)
     pause(100)
     game.gameOver(false)
 })
@@ -13,22 +15,22 @@ let point = { x: sprite.x, y: sprite.y }
 sprite.x = Math.cos(angle) * (point.x - center.x) - Math.sin(angle) * (point.y - center.y) + center.x
     sprite.y = Math.sin(angle) * (point.x - center.x) + Math.cos(angle) * (point.y - center.y) + center.y
 }
+let bg2: Sprite = null
+let bg1: Sprite = null
 let rot_speed = 0
 let obstacle: Sprite = null
 let angle = 0
+let game_over = false
+let backgroundz = -1000
 info.setScore(0)
 let difficulty = 5
-let centerx = 0
-let centery = 0
-let rotatedX = 0
-let rotatedY = 0
 namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 128
     export const ARCADE_SCREEN_HEIGHT = 256
 }
 let reddot = sprites.create(assets.image`reddot`, SpriteKind.Player)
 let bluedot = sprites.create(assets.image`bluedot`, SpriteKind.Player)
-let circle = sprites.create(assets.image`circle`, SpriteKind.Background)
+let circle = sprites.create(assets.image`circle`, SpriteKind.Static)
 circle.z = -10
 circle.sx = 1.05
 circle.sy = 1.05
@@ -47,12 +49,42 @@ game.onUpdateInterval(Math.constrain(1000 - (difficulty + 100) * 3, 10, 1000), f
     }
 })
 game.onUpdate(function () {
-    rot_speed = 8
-    if (controller.left.isPressed()) {
-        rotate(0 - rot_speed, reddot)
-        rotate(0 - rot_speed, bluedot)
-    } else if (controller.right.isPressed()) {
-        rotate(rot_speed, reddot)
-        rotate(rot_speed, bluedot)
+    for (let value of sprites.allOfKind(SpriteKind.Background)) {
+        scaling.scaleByPixels(value, 1.5, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+        scaling.scaleByPixels(value, 1.5, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+        if (value.scale > 10) {
+            sprites.destroy(value)
+        }
     }
+})
+game.onUpdate(function () {
+    rot_speed = 10
+    if (!(game_over)) {
+        if (controller.left.isPressed()) {
+            rotate(0 - rot_speed, reddot)
+            rotate(0 - rot_speed, bluedot)
+        } else if (controller.right.isPressed()) {
+            rotate(rot_speed, reddot)
+            rotate(rot_speed, bluedot)
+        }
+    }
+})
+game.onUpdate(function () {
+    if (game_over) {
+        for (let value of sprites.allOfKind(SpriteKind.Projectile)) {
+            value.vy = 0
+        }
+    }
+})
+game.onUpdateInterval(500, function () {
+    if (backgroundz % 2 == 0) {
+        bg1 = sprites.create(assets.image`bg_dark`, SpriteKind.Background)
+        bg1.z = backgroundz
+        scaling.scaleToPixels(bg1, 2, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    } else {
+        bg2 = sprites.create(assets.image`bg_light`, SpriteKind.Background)
+        bg2.z = backgroundz
+        scaling.scaleToPixels(bg2, 2, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    }
+    backgroundz += 1
 })
