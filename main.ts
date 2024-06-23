a5a6da11b1ca95ab688fb7bcb88c14f297c40d18
Seth Williams <88854453+mr-mph@users.noreply.github.com>
2024-06-23 13:30:39 -0700

    base sprites done, need to add rotation next

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+namespace SpriteKind {
+    export const Background = SpriteKind.create()
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 128
+    export const ARCADE_SCREEN_HEIGHT = 256
+}
+let reddot = sprites.create(assets.image`reddot`, SpriteKind.Background)
+let bluedot = sprites.create(assets.image`bluedot`, SpriteKind.Background)
+let circle = sprites.create(assets.image`circle`, SpriteKind.Background)
+circle.z = -10
+circle.setPosition(64, 200)
+reddot.setPosition(34, 200)
+bluedot.setPosition(96, 200)
