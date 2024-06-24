ef7a91dd09022be63c3c1b8351c0d6bea5eebf20
Seth Williams <88854453+mr-mph@users.noreply.github.com>
2024-06-23 17:02:34 -0700

    added dyanmic sizing for obstacles, adjusted speed & difficulty, made custom text display for score

--- main.ts
+++ main.ts
@@ -17,13 +17,22 @@ sprite.x = Math.cos(angle) * (point.x - center.x) - Math.sin(angle) * (point.y -
 }
 let bg2: Sprite = null
 let bg1: Sprite = null
-let rot_speed = 0
 let obstacle: Sprite = null
+let rot_speed = 0
+let ob_scaling = 0
 let angle = 0
 let game_over = false
-let backgroundz = -1000
-info.setScore(0)
+let textSprite = textsprite.create("0")
+let score_bg = sprites.create(assets.image`score bg`, SpriteKind.Static)
+let score = -1
+scaling.scaleToPixels(textSprite, 13, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+scaling.scaleToPercent(score_bg, 400, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+textSprite.setPosition(105, 10)
+score_bg.setPosition(104, 24)
+score_bg.z = 5
+textSprite.z = 6
 let difficulty = 5
+let backgroundz = -1000
 namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 128
     export const ARCADE_SCREEN_HEIGHT = 256
@@ -37,28 +46,28 @@ circle.sy = 1.05
 circle.setPosition(63, 200)
 reddot.setPosition(32, 200)
 bluedot.setPosition(96, 200)
-game.onUpdateInterval(Math.constrain(1000 - (difficulty + 100) * 3, 10, 1000), function () {
-    obstacle = sprites.createProjectileFromSide(assets.image`obstacle`, 0, difficulty + 100)
-    obstacle.z = -5
-    obstacle.setPosition(randint(32, 96), 0)
-    obstacle.sx = randint(Math.map(Math.abs(64 - obstacle.x), 0, 64, 0.5, 0.8), 0.9)
-    difficulty += 5
-    info.changeScoreBy(1)
-    if (difficulty > 295) {
-        difficulty = 300
-    }
-})
 game.onUpdate(function () {
     for (let value of sprites.allOfKind(SpriteKind.Background)) {
         scaling.scaleByPixels(value, 1.5, ScaleDirection.Uniformly, ScaleAnchor.Middle)
         scaling.scaleByPixels(value, 1.5, ScaleDirection.Uniformly, ScaleAnchor.Middle)
-        if (value.scale > 10) {
+        if (value.scale > 7) {
             sprites.destroy(value)
         }
     }
 })
 game.onUpdate(function () {
-    rot_speed = 10
+    textSprite.setText(convertToText(score))
+    ob_scaling = Math.sin(game.runtime() / 250) * 0.8
+    for (let value of sprites.allOfKind(SpriteKind.Projectile)) {
+        if (game_over) {
+            value.vy = 0
+        } else {
+            scaling.scaleByPercent(value, ob_scaling, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+        }
+    }
+})
+game.onUpdate(function () {
+    rot_speed = 7
     if (!(game_over)) {
         if (controller.left.isPressed()) {
             rotate(0 - rot_speed, reddot)
@@ -69,11 +78,16 @@ game.onUpdate(function () {
         }
     }
 })
-game.onUpdate(function () {
-    if (game_over) {
-        for (let value of sprites.allOfKind(SpriteKind.Projectile)) {
-            value.vy = 0
-        }
+game.onUpdateInterval(Math.constrain(1500 - (difficulty + 50) * 2, 10, 1500), function () {
+    obstacle = sprites.createProjectileFromSide(assets.image`obstacle`, 0, difficulty + 50)
+    obstacle.z = -5
+    obstacle.setPosition(randint(32, 96), 0)
+    obstacle.sx = randint(Math.map(Math.abs(64 - obstacle.x), 0, 64, 0.5, 0.8), 0.9)
+    scaling.scaleByPercent(obstacle, 20, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    difficulty += 2
+    score += 1
+    if (difficulty > 295) {
+        difficulty = 300
     }
 })
 game.onUpdateInterval(500, function () {
